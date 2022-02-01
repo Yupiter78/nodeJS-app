1befd74b95432dea0218ca75ec7e3da4b1c98be2
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-02-01 22:19:36 +0300

    nodejs-app__data_output__5

--- notes.controller.js
+++ notes.controller.js
@@ -1,5 +1,7 @@
 const fs = require("fs/promises");
 const path = require("path");
+const chalk = require("chalk");
+
 const notesPath = path.join(__dirname, "db.json");
 
 async function addNote(title) {
@@ -15,16 +17,28 @@ async function addNote(title) {
     };
     notes.push(note);
     await fs.writeFile(notesPath, JSON.stringify(notes));
+    console.log(chalk.bgGreen("Note was added!"));
 }
-
-addNote("TEST");
 
 async function getNotes() {
     const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
     return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
 }
 
+async function printNotes() {
+    const notes = await getNotes();
+    console.log(chalk.bgBlue("Here is the list of notes:"));
+    notes.forEach((note) => {
+        console.log(
+            "title:",
+            chalk.blue(note.title),
+            "id:",
+            chalk.red(note.id)
+        );
+    });
+}
+
 module.exports = {
     addNote,
-    getNotes
+    printNotes
 };
