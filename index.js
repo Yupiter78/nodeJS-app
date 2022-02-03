4d2f3c68f3054ab6d688fe5e3f1e7fbe987dee1b
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-02-03 03:12:21 +0300

    nodejs-app__automation_with_nodemon__14

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const chalk = require("chalk");
 
 const port = 3000;
 const server = http.createServer((req, res) => {
-    console.log("Server!");
-    res.end("Hello from server!");
+    console.log("request method:", req.method);
+    console.log("request url:", req.url);
+    res.end("Hello from server!!!");
 });
 
 server.listen(port, () => {
